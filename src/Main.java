82cd82e8feba883f28774b317f527f6aa7885d43
ritik <malritik@gmail.com>
2022-06-30 01:16:17 +0530

    DSA

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Node;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,20,-40,30,-10,-60,50};
+        System.out.println(bruteForce(arr));
+    }
+//    private static Pair simpleLinearSearch(int[] arr)
+//    private static Pair divideAndConquer()
+//    private static int[] twoPointerApproach(int[] arr)
+//    private static int dynamicProgramming(int[] arr)
+    private static int kadaneAlgorithm(int[] arr){
+
+    }
+}
